82463884a5ab58dd3d6f165be59d31c11cfeaa54
mondaugen <nicholas.esterer@gmail.com>
2015-10-08 14:26:08 -0400

    Added sinusoid track candidate compare function.

--- sinusoid_analy_track.h
+++ sinusoid_analy_track.h
@@ -35,6 +35,22 @@ typedef struct __sat_assign_nums_candidate_t {
     double distance;
 } sat_assign_nums_candidate_t;
 
+/* Comparison function causes candidates to be sorted from largest to smallest
+ * */
+int sat_assign_nums_candidate_compare(const void *_a, const void *_b)
+{
+    sat_assign_nums_candidate_t *a = (sat_assign_nums_candidate_t*)_a;
+    sat_assign_nums_candidate_t *b = (sat_assign_nums_candidate_t*)_b;
+    double distance_a = a->distance, distance_b = b->distance;
+    if (distance_a < 1.) {
+        distance_a = 1. / distance_a;
+    }
+    if (distance_b < 1.) {
+        distance_b = 1. / distance_b;
+    }
+    return (distance_a < distance_b) - (distance_a > distance_b);
+}
+
 int sat_within_delta_freq_ratio(sat_assign_nums_opt_t *opt,
                                 sinusoid_analy_track_t *a,
                                 sinusoid_analy_track_t *b)
@@ -75,16 +91,24 @@ void sat_assign_nums(sinusoid_analy_track_node_t *track_nodes_k0,
                 continue;
             }
             if (opt->within_delta(opt,
-                                  unmatched_k1[l_k1],
-                                  unmatched_k0[l_k0])
+                        unmatched_k1[l_k1],
+                        unmatched_k0[l_k0])
                     == 0) {
                 candidates[n_candidates++] = &unmatched_k1[l_k1];
             }
         }
+        /* Sort candidates in reverse order (largest delta come first in array,
+         * smallest delta come last) */
+        qsort(candidates,
+                n_candidates,
+                sizeof(sat_assign_nums_candidate_t),
+                sat_assign_nums_candidate_compare);
+        while (ncan-- > 0) {
+            /* Check to see if this candidate is the best match for
+             * unmatched_k0[l_k0] */
+            double _d
+        }
     }
-    /* Sort candidates in reverse order (largest delta come first in array,
-     * smallest delta come last) */
-
 }
     
 #endif /* SINUSOID_ANALY_TRACK_H */
